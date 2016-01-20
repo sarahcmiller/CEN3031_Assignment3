0fbe4a6afe1f8eca104e22639f54c7d14718f010
Sarah Miller <sarahcmiller@ufl.edu>
2016-01-20 15:56:48 -0500

    updated

--- queries.js
+++ queries.js
@@ -1,10 +1,14 @@
 /* Fill out these functions using Mongoose queries*/
 
+
 var findLibraryWest = function() {
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  Listing.findOne({'code':'LBW'}, function(err,obj) { 
+    console.log(obj); 
+  });
 };
 var removeCable = function() {
   /*
@@ -12,17 +16,40 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+  var query = Listing.findOne({'code':'CABL'});
+  Listing.update(
+    { 'code': 'CABL'}, {$pull: {'entries':{'name'}}})
+  
+  if (var i=0; i < listings.entries.length; i++){
+    if (listings.entries[i].code == "CABL") {
+      listings.entries.splice(i,1);
+      break;
+    }
+  }
+
+  console.log(listings);
 };
 var updatePhelpsMemorial = function() {
   /*
     Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+  var query = Listing.findOne({'code':'PHL'});
+  var phlindex;
+  if (var i=0; i < listings.entries.length; i++){
+    if (listings.entries[i].code == "PHL") {
+      phlindex = i;
+      break;
+    }
+  }   
+   listings.entries[phlindex].address = "102 Phelps Lab, Gainesville, FL 32611";
+  console.log(listings);
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+
 };
 
 findLibraryWest();
