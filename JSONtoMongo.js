4c3922e8f9c5e301947fefe3cc8125d4439b6c1e
Robbie Bridgewater <bridgewaterrobbie@gmail.com>
2016-01-20 15:52:28 -0500

    schema working

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,23 +1,81 @@
-'use strict';
-/* 
-  Import modules/files you may need to correctly run the script. 
-  Make sure to save your DB's uri in the config file, then import it with a require statement!
- */
-var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
-    config = require('./config');
-
-/* Connect to your database */
-
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
- */
+  'use strict';
+  /* 
+    Import modules/files you may need to correctly run the script. 
+    Make sure to save your DB's uri in the config file, then import it with a require statement!
+   */
 
 
-/* 
-  Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
- */
+  var has = function(obj, key){
+    return key.split(".").every(function(x) {
+      if(typeof obj != "object" || obj == null || obj == undefined || x in obj)
+        return false;
+      obj = obj[x];
+      return true;
+    });
+  }
+
+  var fs = require('fs'),
+      mongoose = require('mongoose'), 
+      Schema = mongoose.Schema, 
+      Listing = require('./ListingSchema.js'), 
+      config = require('./config');
+
+  /* Connect to your database */
+  mongoose.connect(config.db.uri);
+  mongoose.connection.on('open', function (ref) {
+    console.log('Connected to mongo server.');
+  });
+  /* 
+    Instantiate a mongoose model for each listing object in the JSON file, 
+    and then save it to your Mongo database 
+   */
+  fs.readFile('listings.json','utf8',function(err,data){
+    if(err) throw err;
+    var obj = JSON.parse(data);
+    var code,name,coordinates,address;
+
+  for (var i = 0; i<obj.entries.length; i++) {
+
+    name = obj.entries[i].name;
+    code = obj.entries[i].code;
+    coordinates = undefined;
+    address = undefined;
+
+    //if((has(obj.entries[i],'coordinates.latitude')) && (has(obj.entries[i],'coordinates.longitude')))
+    if(obj.entries[i].coordinates && obj.entries[i].coordinates.latitude)
+  {
+
+    coordinates = {
+      longitude: obj.entries[i].coordinates.longitude,
+      latitude: obj.entries[i].coordinates.latitude
+    }
+   // console.log("coordinates "+coordinates);
+  }
+
+  //if(has(obj.entries[i],'address'))
+  if(obj.entries[i].address)
+  {
+    address = obj.entries[i].address;
+       // console.log("address"+ address);
+
+  }
+
+
+  var listings = new Listing({code: code, name: name, coordinates: coordinates, address: address})
+
+  listings.save(function(err){
+    if(err){
+      throw err
+    }
+  })
+  };
+  //mongoose.connection.
+  });
+
+
+
+
+  /* 
+    Once you've written + run the script, check out your MongoLab database to ensure that 
+    it saved everything correctly. 
+   */
--- ListingSchema.js
+++ ListingSchema.js
@@ -1,17 +1,35 @@
 /* Import mongoose and define any variables needed to create the schema */
 var mongoose = require('mongoose'), 
-    Schema = mongoose.Schema;
+Schema = mongoose.Schema;
 
 /* Create your schema */
 var listingSchema = new Schema({
-  /* your code here */
-
+	/* your code here */
+	code: String,
+	name: String,
+	coordinates: { latitude: Number, longitude: Number},
+	address: String,
+	updated_at: Date,
+	created_at: Date
 });
 
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
-  /* your code here */
+	/* your code here */
+
+	if(this.code === undefined || this.name === undefined){
+		var err = new Error('uh-oh');
+		next(err);
+	}
+
+	if(this.created_at === undefined)
+	{
+		this.created_at = new Date();
+	}
+	this.updated_at = new Date();
+
+	next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
